af920fd429c30a05fa472466f3ad69d159bd9d41
Mary Gualtieri <mgualtieri7@gmail.com>
2017-01-09 21:44:46 -0500

    add blue header

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -3,6 +3,7 @@ import React from 'react';
 const Nav = () => {
   return (
 
+<header>
     <a className="container">
     	<div className="top"></div>
     		<div className="middle"></div>
@@ -14,12 +15,14 @@ const Nav = () => {
         <a href="#"> <li>Search</li></a>
     		<a href="#"> <li>Map</li></a>
     		<a href="#"> <li>Chart</li></a>
-    		<a href="#"> <li>Speaker Registration</li></a>
+    		<a href="#"> <li>Speaker Reg</li></a>
     		<a href="#"> <li>Contact</li></a>
     </ul>
     </a>
 
+          <img src={require('../Code4FtlLogo.png')} className="logo"/>
 
+</header>
 
 
     // <nav>
