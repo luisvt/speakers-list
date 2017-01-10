3c76044d047a8ffbe184f2ec0b10c0c3789da591
kklumpp13 <kklumpp13@gmail.com>
2017-01-10 12:50:11 -0500

    Update intro text

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -5,8 +5,7 @@ const Intro = () => {
   return (
     <section className="bottom-margin">
       <h1>Speaker List for Broward County</h1>
-      <p>In a young and growing tech scene like South Florida it's important to create an environment where tech professionals can network, learn and share knowledge. The first step to building a stronger tech community is to create a resource that makes it easy to find availble speakers in the area.</p>
-      <p>To add your name to the list, please contact <a href="mailto:codeforftl@gmail.com">Code for Fort Lauderdale</a>. And don't be shy - we're interested in hearing what you have to say whether you're a seasoned pro or are still new to the tech community.</p>
+      <p>In a young and growing tech scene like South Florida it's important to foster an environment where tech professionals can network, learn and share knowledge. If you're interested in adding your name to this list, please contact <a href="mailto:codeforftl@gmail.com">Code for Fort Lauderdale</a>. And don't be shy - we're interested in hearing what you have to say whether you're a seasoned pro or are still new to the tech community.</p>
       <p>We'd like to thank <a href="http://wespeaktoo.org/">We Speak Too</a> for the inspiration to start this list. If you can think of any ways to improve it, we'd love to hear your suggestions.</p>
     </section>
   )
