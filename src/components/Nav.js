5b4bf637a34ae4f0f7d9ee641b872d378550e5d8
Mary Gualtieri <mgualtieri7@gmail.com>
2017-01-09 20:48:25 -0500

    added hamburger menu

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -2,17 +2,37 @@ import React from 'react';
 
 const Nav = () => {
   return (
-    <nav>
-      <ul className="navbar">
-        <li className="nav-logo">
-          <img src={require('../Code4FtlLogo.png')} className="logo"/>
-        </li>
-        <li> Search</li>
-        <li> Speaker List</li>
-        <li> Registration Form</li>
-        <li> Contact Us</li>
-      </ul>
-    </nav>
+
+    <a className="container">
+    	<div className="top"></div>
+    		<div className="middle"></div>
+        <div className="bottom"></div>
+
+
+    <ul className="menu">
+    		<a href="#"> <li>Home</li></a>
+        <a href="#"> <li>Search</li></a>
+    		<a href="#"> <li>Map</li></a>
+    		<a href="#"> <li>Chart</li></a>
+    		<a href="#"> <li>Speaker Registration</li></a>
+    		<a href="#"> <li>Contact</li></a>
+    </ul>
+    </a>
+
+
+
+
+    // <nav>
+    //   <ul className="navbar">
+    //     <li className="nav-logo">
+    //       <img src={require('../Code4FtlLogo.png')} className="logo"/>
+    //     </li>
+    //     <li> Search</li>
+    //     <li> Speaker List</li>
+    //     <li> Registration Form</li>
+    //     <li> Contact Us</li>
+    //   </ul>
+    // </nav>
   )
 }
 
