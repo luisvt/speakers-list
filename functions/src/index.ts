6a0c7c1c37fc6657fe9e72b9db74e0ed4f40a9c6
luvargas <luvargas@greathealthworks.com>
2019-05-04 19:53:30 -0400

    fix firestore rules and add createTopic cloud-function

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,9 @@
-// import * as functions from 'firebase-functions';
+import * as functions from 'firebase-functions';
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+export const createTopic = functions.firestore.document('topics/{topicId}').onCreate((change, context) => {
+  const val = change.data() || {};
+
+  if (context && context.auth && context.auth.uid) { val.createdBy = context.auth.uid; }
+
+  return change.ref.set(val);
+});
